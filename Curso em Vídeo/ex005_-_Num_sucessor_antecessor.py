f55b3be59a7f6abe19e7392e33aa6e85aab9c275
Ronald Silva <ronald92silva@gmail.com>
2024-11-05 23:48:15 -0300

    tratando_dados
    exercício para encontrar o número inteiro antes e depois do  digitado

---
+++ Curso em Vídeo/ex005_-_Num_sucessor_antecessor.py
@@ -0,0 +1,5 @@
+num = int(input("Digite um número: "))
+
+print(f'O antecessor desse números é, {num -1} e o sucessor {num +1}.')
+
+print('O antecessor do número {} é, {} e seu sucessor {}.'.format(num, num -1, num + 1))
